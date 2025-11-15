5e3d54e55c57a586214ab45bd8eaf11066827006
Emre Erdem <emreerdem502@gmail.com>
2025-11-15 22:13:24 +0300

    feat(core): walletRouter ekle

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,21 +1,25 @@
 import express, { Request, Response, Application } from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 import { notFound } from "./middlewares/notFound";
 import { errorHandler } from "./middlewares/errorHandler";
 
 import authRouter from "./modules/auth/auth.router";
+import walletRouter from "./modules/wallet/wallet.router";
 
 const app: Application = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (_req: Request, res: Response) => {
   res.status(200).json({ message: "API is running!" });
 });
 
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/wallets", walletRouter);
 
 app.use(notFound);
 app.use(errorHandler);
