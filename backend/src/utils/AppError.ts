3d0e8dbe601d7143097e4e9ef1c55e2b00b6accf
Emre Erdem <emreerdem502@gmail.com>
2025-11-16 23:55:08 +0300

    refactor(AppError): AppError sınıfı iyileştir

--- backend/src/utils/AppError.ts
+++ backend/src/utils/AppError.ts
@@ -1,10 +1,21 @@
+import { iso } from "zod";
+
 export class AppError extends Error {
   statusCode: number;
-  isOperational: boolean;
+  isOperational?: boolean;
 
-  constructor(message: string, statusCode: 500, isOperational: true) {
+  constructor(message: string, statusCode = 500, isOperational?: boolean) {
     super(message);
     this.statusCode = statusCode;
-    this.isOperational = isOperational;
+
+    if (typeof isOperational === "boolean") {
+      this.isOperational === true;
+    } else {
+      this.isOperational = statusCode.toString().startsWith("4");
+    }
+
+    Object.setPrototypeOf(this, new.target.prototype);
+    this.name = new.target.name;
+    Error.captureStackTrace?.(this, this.constructor);
   }
 }
