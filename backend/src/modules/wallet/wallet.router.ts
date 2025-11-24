a2645b127d5e9f577cdfc0934c3888606ee0a96f
Emre Erdem <emreerdem502@gmail.com>
2025-11-24 20:07:18 +0300

    feat(wallet): wallet router güncelle

--- backend/src/modules/wallet/wallet.router.ts
+++ backend/src/modules/wallet/wallet.router.ts
@@ -1,5 +1,18 @@
 import express from "express";
 const Router = express.Router();
+import {
+  createWallet,
+  archiveWallet,
+  getWalletById,
+  getWallets,
+  updateWallet,
+} from "./wallet.controller";
+import { validate } from "../../middlewares/validate";
+import protectRoute from "../../middlewares/protectRoute";
+import { addWalletDto } from "./wallet.dto";
+
+Router.route("/").post(protectRoute, validate(addWalletDto), createWallet);
+Router.route("/:id");
 
 // POST yeni cüzdan ekle
 // GET kullanıcının tüm cüzdanları
