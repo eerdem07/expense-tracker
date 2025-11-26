14baaca060d061403fedebe5e8835f3f63097684
Emre ERDEM <emreerdem502@gmail.com>
2025-11-26 17:40:13 +0300

    fix(wallet): repository hata çözüldü

--- backend/src/modules/wallet/wallet.repository.ts
+++ backend/src/modules/wallet/wallet.repository.ts
@@ -1,10 +1,12 @@
-import { prisma } from "../../db/prisma";
+import { prisma } from '../../db/prisma';
+import { AppError } from '../../utils/AppError';
 
 export function create(data: {
   name: string;
-  type: ["cash", "bank", "credit"];
+  type: 'cash' | 'bank' | 'credit';
   currency: string;
   openingBalance: number;
+  userId: string;
 }) {
   return prisma.wallet.create({
     data: {
@@ -12,14 +14,60 @@ export function create(data: {
       currency: data.currency,
       type: data.type,
       openingBalance: data.openingBalance,
+      userId: data.userId,
     },
   });
 }
 
-export function getWallets(userId: string) {}
+export function getWallets(userId: string) {
+  return prisma.wallet.findMany({
+    where: {
+      userId: userId,
+    },
+  });
+}
 
-export function getWallet(id: string) {}
+export function getWallet(id: string) {
+  return prisma.wallet.findUnique({
+    where: {
+      id: id,
+    },
+  });
+}
 
-export function update(data: {}) {}
+export function update(
+  id: string,
+  data: {
+    name: string;
+    type: 'cash' | 'bank' | 'credit';
+    currency: string;
+    openingBalance: number;
+    userId: string;
+  }
+) {
+  return prisma.wallet.update({
+    where: {
+      id: id,
+    },
+    data: {
+      name: data.name,
+      currency: data.currency,
+      type: data.type,
+      openingBalance: data.openingBalance,
+      userId: data.userId,
+    },
+  });
+}
 
-export function archiveWallet(id: string) {}
+export async function toggleArchiveWallet(walletId: string, userId: string) {
+  return prisma.$transaction(async (tx) => {
+    const wallet = await tx.wallet.findFirstOrThrow({
+      where: { id: walletId, userId: userId },
+    });
+
+    return tx.wallet.update({
+      where: { id: walletId },
+      data: { archived: !wallet.archived },
+    });
+  });
+}
